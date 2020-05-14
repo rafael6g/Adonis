1766dda8bafbc60bf283c7de60c370747cb95176
Rafael Fernandes <rafael.londrina@gmail.com>
2020-05-14 18:17:52 -0300

    Adonis userbackend

---
+++ app/Models/UserAddress.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const Model = use('Model')
+
+class UserAddress extends Model {
+
+}
+
+module.exports = UserAddress
---
+++ start/routes.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const Route = use('Route')
+
+Route.post('users', 'UserController.store').validator('User')
+Route.post('sessions', 'SessionController.store').validator('Session')
+
+Route.post('passwords', 'ForgotPasswordController.store').validator('ForgotPassword')
+Route.put('passwords', 'ForgotPasswordController.update').validator('ResetPassword')
+
+Route.get('/files/:id', 'FileController.show')
+
+Route.group(() => {
+  Route.post('/files', 'FileController.store')
+
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map(
+      [
+        [
+          ['projects.store'],
+          ['Project']
+          /**
+           * para continuar e colocar mais de um,
+           * [
+           *  ['projects.update], ['ProjectUpdate]
+           * ]
+           */
+        ]
+      ]
+    ))
+  Route.resource('projects.tasks', 'TaskController')
+    .apiOnly()
+    .validator(new Map([[['projects.tasks.store'], ['Task']]]))
+}).middleware(['auth'])
